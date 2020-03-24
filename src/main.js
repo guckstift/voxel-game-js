4bc76c4ba2b88073aea68973b735cbbd26c44dee
guckstift <guckstift@posteo.de>
2020-03-25 00:44:07 +0100

    continously load chunks in 3x3 neighbourhood around camera

--- src/main.js
+++ src/main.js
@@ -16,13 +16,8 @@ let crosshairs = new Crosshairs();
 crosshairs.appendToBody();
 
 let map = new Map(display);
-let camera = new Camera(map, 90, 800/600, 0.1, 1000, 1,-1,1, 90,0);
+let camera = new Camera(map, 90, 800/600, 0.1, 1000, 8,8,64, 15,0);
 let controller = new Controller(camera, display);
-
-map.loadChunk(0, 0);
-map.loadChunk(-1, 0);
-map.loadChunk(0, 1);
-map.loadChunk(-1, 1);
 
 let sun = new Vector(0,0,1);
 
@@ -32,6 +27,15 @@ let picker = new Picker(display, map);
 
 display.onframe = () =>
 {
+	let cx = Math.floor(camera.pos.x / 16);
+	let cy = Math.floor(camera.pos.y / 16);
+	
+	for(let y = cy - 1; y <= cy + 1; y++) {
+		for(let x = cx - 1; x <= cx + 1; x++) {
+			map.loadChunk(x, y);
+		}
+	}
+	
 	controller.update(1/60);
 	
 	camera.aspect = display.getAspect();
