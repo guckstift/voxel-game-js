226ddd3ddf0bd6339188789e1a36c5b3293c0a40
guckstift <guckstift@posteo.de>
2020-03-29 16:46:07 +0200

    implement greedy meshing

--- src/chunk.js
+++ src/chunk.js
@@ -13,14 +13,21 @@ let vert = `
 	attribute vec3 pos;
 	attribute vec3 norm;
 	attribute vec2 uv;
+	attribute float face;
 	attribute float ao;
 	varying mediump vec2 vUv;
+	varying mediump vec2 texOffs;
 	varying mediump float factor;
 	
 	void main()
 	{
 		gl_Position = proj * view * model * vec4(pos, 1);
 		vUv = uv;
+		
+		float texX = mod(face, 16.0);
+		float texY = floor(face / 16.0);
+		
+		texOffs = vec2(texX, texY) / 16.0;
 		
 		float diffuse = max(0.0, dot(sun, norm));
 		float ambient = (4.0 - ao) * 0.25;
@@ -32,11 +39,14 @@ let vert = `
 let frag = `
 	uniform sampler2D tex;
 	varying mediump vec2 vUv;
+	varying mediump vec2 texOffs;
 	varying mediump float factor;
 	
 	void main()
 	{
-		gl_FragColor = texture2D(tex, vUv);
+		mediump vec2 texCoord = texOffs + fract(vUv) / 16.0;
+		
+		gl_FragColor = texture2D(tex, texCoord);
 		gl_FragColor.rgb *= factor;
 	}
 `;
@@ -113,47 +123,58 @@ export default class Chunk
 		let gl = this.gl;
 		let mesh = [];
 		
-		this.remeshSide(mesh, -1, 0, 0, [0,1,0], [0,0,0], [0,1,1], [0,0,1], 0, // left
+		this.remeshSide(mesh, -1, 0, 0, 0, // left
+			1, 2, 0, false, true, false,
 			[-1,+1,+1], [-1, 0,+1], [-1,-1,+1],
 			[-1,+1, 0],             [-1,-1, 0],
 			[-1,+1,-1], [-1, 0,-1], [-1,-1,-1]);
-		this.remeshSide(mesh,  0,-1, 0, [0,0,0], [1,0,0], [0,0,1], [1,0,1], 1, // front
+		this.remeshSide(mesh,  0,-1, 0, 1, // front
+			0, 2, 1, false, false, false,
 			[-1,-1,+1], [ 0,-1,+1], [+1,-1,+1],
 			[-1,-1, 0],             [+1,-1, 0],
 			[-1,-1,-1], [ 0,-1,-1], [+1,-1,-1]);
-		this.remeshSide(mesh,  0, 0,-1, [0,1,0], [1,1,0], [0,0,0], [1,0,0], 2, // bottom
+		this.remeshSide(mesh,  0, 0,-1, 2, // bottom
+			0, 1, 2, false, true, false,
 			[-1,-1,-1], [ 0,-1,-1], [+1,-1,-1],
 			[-1, 0,-1],             [+1, 0,-1],
 			[-1,+1,-1], [ 0,+1,-1], [+1,+1,-1]);
-		this.remeshSide(mesh, +1, 0, 0, [1,0,0], [1,1,0], [1,0,1], [1,1,1], 3, // right
+		this.remeshSide(mesh, +1, 0, 0, 3, // right
+			1, 2, 0, false, false, false,
 			[+1,-1,+1], [+1, 0,+1], [+1,+1,+1],
 			[+1,-1, 0],             [+1,+1, 0],
 			[+1,-1,-1], [+1, 0,-1], [+1,+1,-1]);
-		this.remeshSide(mesh,  0,+1, 0, [1,1,0], [0,1,0], [1,1,1], [0,1,1], 4, // back
+		this.remeshSide(mesh,  0,+1, 0, 4, // back
+			0, 2, 1, true, false, false,
 			[+1,+1,+1], [ 0,+1,+1], [-1,+1,+1],
 			[+1,+1, 0],             [-1,+1, 0],
 			[+1,+1,-1], [ 0,+1,-1], [-1,+1,-1]);
-		this.remeshSide(mesh,  0, 0,+1, [0,0,1], [1,0,1], [0,1,1], [1,1,1], 5, // top
+		this.remeshSide(mesh,  0, 0,+1, 5, // top
+			0, 1, 2, false, false, false,
 			[-1,+1,+1], [ 0,+1,+1], [+1,+1,+1],
 			[-1, 0,+1],             [+1, 0,+1],
 			[-1,-1,+1], [ 0,-1,+1], [+1,-1,+1]);
 		
 		this.buffer.update(new Float32Array(mesh));
-		this.count = mesh.length / 9;
+		this.count = mesh.length / 10;
 	}
 	
-	remeshSide(mesh, nx, ny, nz, p0, p1, p2, p3, fid, aov0, aov1, aov2, aov3, aov4, aov5, aov6, aov7)
-	{
+	remeshSide(
+		mesh, nx, ny, nz, fid, ax0, ax1, ax2, fx, fy, fz, aov0, aov1, aov2, aov3, aov4, aov5, aov6, aov7
+	) {
+		let map = [];
+		
 		for(let z=0, i=0; z<256; z++) {
 			for(let y=0; y<16; y++) {
 				for(let x=0; x<16; x++, i++) {
-					let block = (
-						this.getBlock(x, y, z) > 0 && this.getBlock(x + nx, y + ny, z + nz) === 0
-						? this.getBlock(x, y, z)
+					let block = this.getBlock(x, y, z);
+					
+					map[i] = (
+						block > 0 && this.getBlock(x + nx, y + ny, z + nz) === 0
+						? block
 						: 0
 					);
 					
-					if(block > 0) {
+					if(map[i] > 0) {
 						let ao0 = this.getOcclusion(
 							[x + aov3[0], y + aov3[1], z + aov3[2]],
 							[x + aov5[0], y + aov5[1], z + aov5[2]],
@@ -178,16 +199,100 @@ export default class Chunk
 							[x + aov1[0], y + aov1[1], z + aov1[2]],
 						);
 						
-						let face = blocks[block].faces[fid];
-						let fx = face % 16 / 16;
-						let fy = Math.floor(face / 16) / 16;
-						let v0 = [x + p0[0], y + p0[1], z + p0[2], nx, ny, nz, 0/16 + fx, 1/16 + fy, ao0];
-						let v1 = [x + p1[0], y + p1[1], z + p1[2], nx, ny, nz, 1/16 + fx, 1/16 + fy, ao1];
-						let v2 = [x + p2[0], y + p2[1], z + p2[2], nx, ny, nz, 0/16 + fx, 0/16 + fy, ao2];
-						let v3 = [x + p3[0], y + p3[1], z + p3[2], nx, ny, nz, 1/16 + fx, 0/16 + fy, ao3];
-						let flip = ao0 + ao3 < ao1 + ao2;
+						map[i] |= ao0 << 8 | ao1 << 10 | ao2 << 12 | ao3 << 14;
+					}
+				}
+			}
+		}
+		
+		let a = [fx ? 15 : 0,  fy ? 15 : 0,  fz ? 255 : 0];
+		let b = [fx ? -1 : 16, fy ? -1 : 16, fz ? -1  : 256];
+		let s = [fx ? -1 : +1, fy ? -1 : +1, fz ? -1  : +1];
+		let i = [0,0,0];
+		let j = [0,0,0];
+		let k = [0,0,0];
+		
+		for(i[ax2] = a[ax2]; i[ax2] !== b[ax2]; i[ax2] += s[ax2]) {
+			for(i[ax1] = a[ax1]; i[ax1] !== b[ax1]; i[ax1] += s[ax1]) {
+				for(i[ax0] = a[ax0]; i[ax0] !== b[ax0]; i[ax0] += s[ax0]) {
+					let I = i[0] + i[1] * 16 + i[2] * 16 * 16;
+					let val = map[I];
+					
+					if(val > 0) {
+						j[0] = i[0];
+						j[1] = i[1];
+						j[2] = i[2];
+						k[0] = i[0];
+						k[1] = i[1];
+						k[2] = i[2];
 						
-						if(flip) {
+						for(j[ax0] = i[ax0] + s[ax0]; j[ax0] !== b[ax0]; j[ax0] += s[ax0]) {
+							let J = j[0] + j[1] * 16 + j[2] * 16 * 16;
+							
+							if(map[J] !== val) {
+								break;
+							}
+							
+							map[J] = 0;
+						}
+						
+						outer:
+						for(k[ax1] = i[ax1] + s[ax1]; k[ax1] !== b[ax1]; k[ax1] += s[ax1]) {
+							for(k[ax0] = i[ax0]; k[ax0] !== j[ax0]; k[ax0] += s[ax0]) {
+								let K = k[0] + k[1] * 16 + k[2] * 16 * 16;
+								
+								if(map[K] !== val) {
+									break outer;
+								}
+							}
+							
+							for(k[ax0] = i[ax0]; k[ax0] !== j[ax0]; k[ax0] += s[ax0]) {
+								let K = k[0] + k[1] * 16 + k[2] * 16 * 16;
+								
+								map[K] = 0;
+							}
+						}
+						
+						let block = val & 0xff;
+						let face = blocks[block].faces[fid];
+						let ao0 = val >>  8 & 3;
+						let ao1 = val >> 10 & 3;
+						let ao2 = val >> 12 & 3;
+						let ao3 = val >> 14 & 3;
+						let quadw = (j[ax0] - i[ax0]) * s[ax0];
+						let quadh = (k[ax1] - i[ax1]) * s[ax1];
+						let v0 = [...i, nx, ny, nz,     0, quadh, face, ao0];
+						let v1 = [...i, nx, ny, nz, quadw, quadh, face, ao1];
+						let v2 = [...i, nx, ny, nz,     0,     0, face, ao2];
+						let v3 = [...i, nx, ny, nz, quadw,     0, face, ao3];
+						
+						v1[ax0] = j[ax0];
+						v3[ax0] = j[ax0];
+						v2[ax1] = k[ax1];
+						v3[ax1] = k[ax1];
+						
+						if(s[ax0] < 0) {
+							v0[ax0] ++;
+							v1[ax0] ++;
+							v2[ax0] ++;
+							v3[ax0] ++;
+						}
+						
+						if(s[ax1] < 0) {
+							v0[ax1] ++;
+							v1[ax1] ++;
+							v2[ax1] ++;
+							v3[ax1] ++;
+						}
+						
+						if(nx > 0 || ny > 0 || nz > 0) {
+							v0[ax2] ++;
+							v1[ax2] ++;
+							v2[ax2] ++;
+							v3[ax2] ++;
+						}
+						
+						if(ao0 + ao3 < ao1 + ao2) {
 							mesh.push(...v1, ...v3, ...v0, ...v0, ...v3, ...v2);
 						}
 						else {
@@ -212,10 +317,11 @@ export default class Chunk
 	{
 		let shader = this.shader;
 		
-		shader.assignFloatAttrib("pos",  this.buffer, 3, 9, 0);
-		shader.assignFloatAttrib("norm", this.buffer, 3, 9, 3);
-		shader.assignFloatAttrib("uv",   this.buffer, 2, 9, 6);
-		shader.assignFloatAttrib("ao",   this.buffer, 1, 9, 8);
+		shader.assignFloatAttrib("pos",  this.buffer, 3, 10, 0);
+		shader.assignFloatAttrib("norm", this.buffer, 3, 10, 3);
+		shader.assignFloatAttrib("uv",   this.buffer, 2, 10, 6);
+		shader.assignFloatAttrib("face", this.buffer, 1, 10, 8);
+		shader.assignFloatAttrib("ao",   this.buffer, 1, 10, 9);
 		shader.use();
 		shader.assignMatrix("proj", camera.proj);
 		shader.assignMatrix("view", camera.view);
