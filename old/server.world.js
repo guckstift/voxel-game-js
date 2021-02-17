73f8b92207a86a2a437dc3f4bf08b59976ee5abb
guckstift <guckstift@posteo.de>
2021-02-17 22:43:16 +0100

    remove old stuff

--- old/server.world.js
+++
@@ -1,8 +0,0 @@
-export class World
-{
-	constructor(store, server)
-	{
-		this.store = store;
-		this.server = server;
-	}
-}
--- server/main.js
+++
@@ -1,3 +0,0 @@
-import {Server} from "./server.js";
-
-let server = new Server(process.argv[2] ? parseInt(process.argv[2]) : 12345);
